acc8433901fc7469e01a5064e047fc92b570d2d6
Morven Lewis-Everley <morven@i-lateral.com>
2012-08-28 12:53:51 +0100

    Setup basic hierarchy browsing on ProductCategories

--- code/admin/ProductAdmin.php
+++ code/admin/ProductAdmin.php
@@ -14,4 +14,80 @@ class ProductAdmin extends ModelAdmin {
     public function init() {
         parent::init();
     }
+	
+	function getEditForm($id = null, $fields = null) {
+    	$form = parent::getEditForm($id, $fields);
+		$params = $this->request->requestVar('q');
+		
+		// Alterations for Hiarachy on product cataloge
+		if($this->modelClass == 'ProductCategory') {
+			$fields = $form->Fields();
+			$gridField = $fields->fieldByName('ProductCategory');
+			
+			// Tidy up category config
+			$field_config = $gridField->getConfig();
+			$field_config
+	            ->removeComponentsByType('GridFieldExportButton')
+	            ->removeComponentsByType('GridFieldPrintButton')
+				->addComponents(
+					GridFieldLevelup::create($this->currentCategoryID())->setLinkSpec('admin/products/ProductCategory/?ParentID=%d')
+				);
+			
+			// Get GridField list
+			$categories = ProductCategory::get();
+			$categories->where("ParentID = {$this->currentCategoryID()}");
+			
+			// Update list
+			$gridField->setList($categories);
+			
+			// Find data colums, so we can add link to view children
+			$columns = $gridField->getConfig()->getComponentByType('GridFieldDataColumns');
+
+			// Don't allow navigating into children nodes on filtered lists
+			$fields = array(
+				'Title' => 'Title',
+			);
+
+			if(!$params) {
+				$fields = array_merge(array('listChildrenLink' => ''), $fields);
+			}
+	
+			$columns->setDisplayFields($fields);
+			$columns->setFieldCasting(array('Title' => 'HTMLText'));
+
+			$controller = $this;
+			$columns->setFieldFormatting(array(
+				'listChildrenLink' => function($value, &$item) use($controller) {
+					$num = $item ? $item->numChildren() : null;
+					if($num) {
+						return sprintf(
+							'<a class="list-children-link" data-pjax-target="ListViewForm" href="%s?ParentID=%d">%s</a>',
+							$controller->Link(),
+							$item->ID,
+							$num
+						);
+					}
+				}
+			));
+			
+			
+		}
+		
+        return $form;
+    }
+	
+	/**
+	 * Return fake-ID "root" if no ID is found (needed to upload files into the root-folder)
+	 */
+	public function currentCategoryID() {
+		if(is_numeric($this->request->requestVar('ParentID'))) {
+			return $this->request->requestVar('ParentID');
+		} elseif (isset($this->urlParams['ParentID']) && is_numeric($this->urlParams['ID'])) {
+			return $this->urlParams['ID'];
+		} elseif(Session::get("{$this->class}.currentCategory")) {
+			return Session::get("{$this->class}.currentCategory");
+		} else {
+			return 0;
+		}
+	}
 }
--- code/model/ProductCategory.php
+++ code/model/ProductCategory.php
@@ -13,9 +13,39 @@ class ProductCategory extends DataObject {
 		'Products' => 'Product'
 	);
 
-	static $extensions = array(
+	public static $extensions = array(
 		"Hierarchy"
 	);
+
+	public function getCMSFields() {
+		$fields = parent::getCMSFields();
+		
+		$fields->addFieldToTab('Root.Main', new TextField('Title'));
+		
+		// If record is just created, check for parent ID in URL and set appropriately
+		$parentField = new TreeDropdownField('ParentID', 'Parent Category', 'ProductCategory');
+
+		$fields->addFieldToTab('Root.Main', $parentField);
+		
+		return $fields;
+	}
+	
+	public function populateDefaults() {
+		$parentParam = Controller::curr()->request->requestVar('ParentID');
+		
+		if($parentParam && is_numeric($parentParam))
+			$this->ParentID = $parentParam;
+		
+		parent::populateDefaults();		
+	}
+	
+	public function getChildren() {
+		if($this->getChildren()->exists()) {
+			return 1;
+		} else {
+			return 0;
+		}
+	}
 
     public function canCreate($member = null) {
         return true;
