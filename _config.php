51ef9adfaee68d582edebcf61a98493b11ee7e16
Morven Lewis-Everley <morven@i-lateral.com>
2012-10-05 15:29:38 +0100

    Setup routing using SS3.0 YML

--- _config.php
+++ _config.php
@@ -1,13 +1,4 @@
 <?php
-// Setup rules for content controllers
-Director::addRules(100, array(
-    Cart_Controller::$url_segment           => 'Cart_Controller',
-    Template_Controller::$url_segment       => 'Template_Controller',
-    Checkout_Controller::$url_segment       => 'Checkout_Controller',
-    Summary_Controller::$url_segment        => 'Summary_Controller',
-    OrderResponse_Controller::$url_segment  => 'OrderResponse_Controller'
-));
-
 // Extentions
 Object::add_extension('SiteConfig', 'Commerce_SiteConfig');
 Object::add_extension('ContentController', 'Commerce_Controller');
