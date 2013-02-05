e55f52636e6bf5502d21239219dab4558552ddc3
Morven Lewis-Everley <morven@i-lateral.com>
2013-02-05 15:07:31 +0000

    Add default translation file, required by SS3

---
+++ lang/js/en_US.js
@@ -0,0 +1,8 @@
+if(typeof(ss) == 'undefined' || typeof(ss.i18n) == 'undefined') {
+  console.error('Class ss.i18n not defined');
+} else {
+	ss.i18n.addDictionary('en_US', {
+		// Cart Page Translations
+		'COMMERCE.CART.NOPOSTAGE' : 'You must select a postage area to continue'
+	});
+}
