2583db7d4e27f620f1f9c8054868478723f8bc0d
Morven Lewis-Everley <morven@i-lateral.com>
2013-05-16 17:08:37 +0100

    Add slightly more sensible fields to the commerce admin

--- code/extensions/Commerce_SiteConfig.php
+++ code/extensions/Commerce_SiteConfig.php
@@ -20,20 +20,20 @@ class Commerce_SiteConfig extends DataExtension {
         'ProcessingEmailAddress'=> "Text",
         'SendDispatchedEmail'   => "Enum('No,Customer,Vendor,Both','Customer')",
         'DispatchedEmailAddress'=> "Text",
-        'VendorEmailFooter'		=> "Text"
+        'VendorEmailFooter'     => "Text"
     );
-    
+
     public static $has_one = array(
         'NoProductImage'    => 'Image',
         'Currency'          => 'CommerceCurrency',
         'Weight'            => 'ProductWeight'
     );
-    
+
     public static $has_many = array(
         'PostageAreas'      => 'PostageArea',
         'PaymentMethods'    => 'CommercePaymentMethod'
     );
-    
+
     public function sendCommerceEmail($recipient, $status) {
         if($recipient == 'Customer')
                 $array = array('Customer', 'Both');
@@ -41,7 +41,7 @@ class Commerce_SiteConfig extends DataExtension {
                 $array = array('Vendor', 'Both');
         else
                 $array = array();
-            
+
         if($status == 'paid' && in_array($this->owner->SendPaidEmail, $array))
                 return true;
         elseif($status == 'processing' && in_array($this->owner->SendProcessingEmail, $array))
@@ -51,65 +51,65 @@ class Commerce_SiteConfig extends DataExtension {
         else
                 return false;
     }
-    
+
     public function updateCMSFields(FieldList $fields) {
-        
+
         // Ecommerce Fields
-        $fields->addFieldToTab('Root.Main', HeaderField::create('CommerceHeader', 'Ecommerce', 2));        
-        
+        $fields->addFieldToTab('Root.Main', HeaderField::create('CommerceHeader', 'Ecommerce', 2));
+
         // Compress default commerce settings
         $contact_fields = ToggleCompositeField::create('ContactDetails', 'Contact Details',
                 array(
-					TextField::create('ContactEmail', 'Email Address'),
-					TextField::create('ContactPhone', 'Phone Number')
+                    TextField::create('ContactEmail', 'Email Address'),
+                    TextField::create('ContactPhone', 'Phone Number')
                 )
         )->setHeadingLevel(4);
-        
+
         // Compress default commerce settings
         $settings_fields = ToggleCompositeField::create('CommerceSettings', 'Default Settings',
                 array(
-					TextField::create('OrderPrefix', 'Short code that can appear at the start of order numbers', null, 9),
-					DropdownField::create('CurrencyID', 'Currency to use', CommerceCurrency::get()->map(), $this->owner->CurrencyID)->setEmptyString('Please Select'),
-					DropdownField::create('WeightID', 'Weight to use', ProductWeight::get()->map(), $this->owner->WeightID)->setEmptyString('Please Select'),
-					UploadField::create('NoProductImage','Overwrite default "image unavailable" image')
+                    TextField::create('OrderPrefix', 'Short code that can appear at the start of order numbers', null, 9),
+                    DropdownField::create('CurrencyID', 'Currency to use', CommerceCurrency::get()->map(), $this->owner->CurrencyID)->setEmptyString('Please Select'),
+                    DropdownField::create('WeightID', 'Weight to use', ProductWeight::get()->map(), $this->owner->WeightID)->setEmptyString('Please Select'),
+                    UploadField::create('NoProductImage','Overwrite default "image unavailable" image')
                 )
         )->setHeadingLevel(4);
-        
-        
+
+
         // Compress shopping cart settings
         $cart_fields = ToggleCompositeField::create('CartProcess', 'Cart and Checkout Content',
                 array(
-					HtmlEditorField::create('CartCopy', 'Shopping cart')->setRows(15)->addExtraClass('stacked'),
-					TextAreaField::create('SuccessCopy', 'Order success page')->setRows(4)->setColumns(30)->addExtraClass('stacked'),
-					TextAreaField::create('FailerCopy', 'Order failer page')->setRows(4)->setColumns(30)->addExtraClass('stacked')              
+                    HtmlEditorField::create('CartCopy', 'Shopping cart')->setRows(15)->addExtraClass('stacked'),
+                    TextAreaField::create('SuccessCopy', 'Order success page')->setRows(4)->setColumns(30)->addExtraClass('stacked'),
+                    TextAreaField::create('FailerCopy', 'Order failer page')->setRows(4)->setColumns(30)->addExtraClass('stacked')
                 )
         )->setHeadingLevel(4);
-        
+
         // Compress email alerts
         $email_fields = ToggleCompositeField::create('EmailAlerts', 'Email Alerts',
                 array(
-					TextField::create('EmailFromAddress', 'Send commerce notifications from?'),
-					LiteralField::create('OrderPlacedHeader', '<div class="field"><h4>When an order is placed</h4></div>'),
-					DropdownField::create('SendPaidEmail', 'Send emails to', $this->owner->dbObject('SendPaidEmail')->enumValues()),
-					TextField::create('PaidEmailAddress', 'From address'),
-					LiteralField::create('OrderProcessingHeader', '<div class="field"><h4>When an order is marked as processing</h4></div>'),
-					DropdownField::create('SendProcessingEmail', 'Send emails to', $this->owner->dbObject('SendProcessingEmail')->enumValues()),
-					TextField::create('ProcessingEmailAddress', 'From address'),
-					LiteralField::create('OrderPlacedHeader', '<div class="field"><h4>When an order is marked as dispatched</h4></div>'),
-					DropdownField::create('SendDispatchedEmail', 'Send emails to', $this->owner->dbObject('SendDispatchedEmail')->enumValues()),
-					TextField::create('DispatchedEmailAddress', 'From address'),
-					LiteralField::create('FooterContent', '<div class="field"><h4>Footer Content</h4></div>'),
-					TextareaField::create('VendorEmailFooter', 'Add custom content to the footer of vendor emails?')
+                    TextField::create('EmailFromAddress', 'Send commerce notifications from?'),
+                    LiteralField::create('OrderPlacedHeader', '<div class="field"><h4>When an order is placed</h4></div>'),
+                    DropdownField::create('SendPaidEmail', 'Send emails to', $this->owner->dbObject('SendPaidEmail')->enumValues()),
+                    TextField::create('PaidEmailAddress', 'Vendor address'),
+                    LiteralField::create('OrderProcessingHeader', '<div class="field"><h4>When an order is marked as processing</h4></div>'),
+                    DropdownField::create('SendProcessingEmail', 'Send emails to', $this->owner->dbObject('SendProcessingEmail')->enumValues()),
+                    TextField::create('ProcessingEmailAddress', 'Vendor address'),
+                    LiteralField::create('OrderPlacedHeader', '<div class="field"><h4>When an order is marked as dispatched</h4></div>'),
+                    DropdownField::create('SendDispatchedEmail', 'Send emails to', $this->owner->dbObject('SendDispatchedEmail')->enumValues()),
+                    TextField::create('DispatchedEmailAddress', 'Vendor address'),
+                    LiteralField::create('FooterContent', '<div class="field"><h4>Footer Content</h4></div>'),
+                    TextareaField::create('VendorEmailFooter', 'Add custom content to the footer of vendor emails?')
                 )
         )->setHeadingLevel(4);
-        
+
         // Add config sets
         $fields->addFieldToTab('Root.Main', $contact_fields);
         $fields->addFieldToTab('Root.Main', $settings_fields);
         $fields->addFieldToTab('Root.Main', $cart_fields);
         $fields->addFieldToTab('Root.Main', $email_fields);
-        
-		// Postage
+
+        // Postage
         $postage_config = GridFieldConfig::create()->addComponents(
             new GridFieldToolbarHeader(),
             new GridFieldAddNewButton('toolbar-header-right'),
@@ -120,12 +120,12 @@ class Commerce_SiteConfig extends DataExtension {
             new GridFieldDeleteAction(),
             new GridFieldDetailForm()
         );
-		
+
         $postage_table = GridField::create('PostageAreas','PostageArea',$this->owner->PostageAreas(), $postage_config);
-		
+
         $fields->addFieldToTab('Root.Postage', $postage_table);
-        
-		// Payment Methods
+
+        // Payment Methods
         $payment_config = GridFieldConfig::create()->addComponents(
             new GridFieldToolbarHeader(),
             new GridFieldAddNewButton('toolbar-header-right'),
@@ -137,12 +137,12 @@ class Commerce_SiteConfig extends DataExtension {
             new GridFieldDetailForm()
         );
         $payment_table = GridField::create('PaymentMethods','CommercePaymentMethod',$this->owner->PaymentMethods(), $payment_config);
-		
+
         $fields->addFieldToTab('Root.Payments', $payment_table);
     }
-    
+
     public function requireDefaultRecords() {
-    
+
         // If "no product image" is not in DB, add it
         if(!Image::get()->filter('Name','no-image.png')->first()) {
             $image = new Image();
@@ -151,8 +151,8 @@ class Commerce_SiteConfig extends DataExtension {
             $image->Filename = 'commerce/images/no-image.png';
             $image->ShowInSearch = 0;
             $image->write();
-        
-			DB::alteration_message('No image file added to DB', 'created');
+
+            DB::alteration_message('No image file added to DB', 'created');
         }
     }
 }
--- code/model/Order.php
+++ code/model/Order.php
@@ -27,16 +27,16 @@ class Order extends DataObject {
         'EmailDispatchSent' => 'Boolean',
         'Status'            => "Enum('incomplete,failed,paid,processing,dispatched','incomplete')"
     );
-    
+
     public static $has_one = array(
         'Postage' => 'PostageArea'
     );
-    
+
     public static $has_many = array(
         'Items' => 'OrderItem'
     );
-    
-    // Cast method calls nicely 
+
+    // Cast method calls nicely
     public static $casting = array(
         'BillingAddress'    => 'Text',
         'DeliveryAddress'   => 'Text',
@@ -46,11 +46,11 @@ class Order extends DataObject {
         'ItemSummary'       => 'HTMLText',
         'TranslatedStatus'  => 'Varchar'
     );
-    
+
     public static $defaults = array(
         'EmailDispatchSent' => 0
     );
-    
+
     public static $summary_fields = array(
         "OrderNumber" => "Order Number",
         "BillingFirstnames" => "First Name(s)",
@@ -60,16 +60,16 @@ class Order extends DataObject {
         "Created" => "Created"
     );
 
-	static $default_sort = "Created DESC";
-    
+    static $default_sort = "Created DESC";
+
     public function getCMSFields() {
         $fields = parent::getCMSFields();
-        
+
         // Remove defailt item admin
         $fields->removeByName('Items');
         $fields->removeByName('EmailDispatchSent');
         $fields->removeByName('PostageID');
-        
+
         // Remove Billing Details
         $fields->removeByName('BillingFirstnames');
         $fields->removeByName('BillingSurname');
@@ -78,7 +78,7 @@ class Order extends DataObject {
         $fields->removeByName('BillingCity');
         $fields->removeByName('BillingPostCode');
         $fields->removeByName('BillingCountry');
-        
+
         // Remove Delivery Details
         $fields->removeByName('DeliveryFirstnames');
         $fields->removeByName('DeliverySurname');
@@ -87,7 +87,7 @@ class Order extends DataObject {
         $fields->removeByName('DeliveryCity');
         $fields->removeByName('DeliveryPostCode');
         $fields->removeByName('DeliveryCountry');
-        
+
         // Add non-editable order number
         $ordernum_field = TextField::create('OrderNumber')
             ->setReadonly(true)
@@ -99,32 +99,32 @@ class Order extends DataObject {
         $lastedited_field = TextField::create('LastEdited', 'Last time order was saved')
             ->setReadonly(true)
             ->performReadonlyTransformation();
-        
+
         $created_field = TextField::create('Created')
             ->setReadonly(true)
             ->performReadonlyTransformation();
-        
+
         $fields->addFieldToTab('Root.Main', $created_field, 'EmailDispatchSent');
         $fields->addFieldToTab('Root.Main', $lastedited_field, 'EmailDispatchSent');
-        
+
         // Load basic list of items
         $item_config = GridFieldConfig::create()->addComponents(
             new GridFieldSortableHeader(),
             new GridFieldDataColumns(),
             new GridFieldFooter()
         );
-		
+
         $item_field = ToggleCompositeField::create('OrderItems', 'Order Items',
-			array(
+            array(
                 GridField::create('Items',null,$this->Items(), $item_config)
-			)
-		)->setHeadingLevel(4);
-        
+            )
+        )->setHeadingLevel(4);
+
         $fields->addFieldToTab('Root.Main', $item_field);
-        		
+
         // Structure billing details
         $billing_fields = ToggleCompositeField::create('BillingDetails', 'Billing Details',
-			array(
+            array(
                 TextField::create('BillingFirstnames', 'First Name(s)'),
                 TextField::create('BillingSurname', 'Surname'),
                 TextField::create('BillingAddress1', 'Address 1'),
@@ -132,14 +132,14 @@ class Order extends DataObject {
                 TextField::create('BillingCity', 'City'),
                 TextField::create('BillingPostCode', 'Post Code'),
                 TextField::create('BillingCountry', 'Country')
-			)
-		)->setHeadingLevel(4);
-        
+            )
+        )->setHeadingLevel(4);
+
         $fields->addFieldToTab('Root.Main', $billing_fields);
-        
+
         // Structure delivery details
         $delivery_fields = ToggleCompositeField::create('DeliveryDetails', 'Delivery Details',
-			array(
+            array(
                 DropdownField::create('PostageID', 'Postage', PostageArea::get()->map('ID', 'Location')),
                 TextField::create('DeliveryFirstnames', 'First Name(s)'),
                 TextField::create('DeliverySurname', 'Surname'),
@@ -148,156 +148,156 @@ class Order extends DataObject {
                 TextField::create('DeliveryCity', 'City'),
                 TextField::create('DeliveryPostCode', 'Post Code'),
                 TextField::create('DeliveryCountry', 'Country')
-			)
-		)->setHeadingLevel(4);
-        
+            )
+        )->setHeadingLevel(4);
+
         $fields->addFieldToTab('Root.Main', $delivery_fields);
-        
+
         return $fields;
     }
-    
+
     public function getPostageCost() {
         return $this->Postage()->Cost;
     }
-    
+
     public function getBillingAddress() {
         $address = ($this->BillingAddress1) ? $this->BillingAddress1 . ",\n" : '';
         $address .= ($this->BillingAddress2) ? $this->BillingAddress2 . ",\n" : '';
         $address .= ($this->BillingCity) ? $this->BillingCity . ",\n" : '';
         $address .= ($this->BillingPostCode) ? $this->BillingPostCode . ",\n" : '';
         $address .= ($this->BillingCountry) ? $this->BillingCountry : '';
-        
+
         return $address;
     }
-    
+
     public function getDeliveryAddress() {
         $address = ($this->DeliveryAddress1) ? $this->DeliveryAddress1 . ",\n" : '';
         $address .= ($this->DeliveryAddress2) ? $this->DeliveryAddress2 . ",\n" : '';
         $address .= ($this->DeliveryCity) ? $this->DeliveryCity . ",\n" : '';
         $address .= ($this->DeliveryPostCode) ? $this->DeliveryPostCode . ",\n" : '';
         $address .= ($this->DeliveryCountry) ? $this->DeliveryCountry : '';
-        
+
         return $address;
     }
-    
+
     public function getOrderTotal() {
         $total = $this->SubTotal;
-        
+
         // Add postage
         if(is_int((int)Session::get('PostageID')) && (int)Session::get('PostageID') > 0)
             $total += DataObject::get_by_id('PostageArea', Session::get('PostageID'))->Cost;
-        
+
         return number_format($total,2);
     }
-    
+
     public function getItemSummary() {
         $return = '';
-        
+
         foreach($this->Items() as $item) {
             $return .= "{$item->Quantity} x {$item->Title};\n";
         }
-        
+
         return $return;
     }
-    
+
     public function getSubTotal() {
         $total = 0;
-        
+
         // Calculate total from items in the list
         foreach($this->Items() as $order_item) {
             $total += $order_item->getTotal();
         }
-        
+
         return $total;
     }
-    
+
     public function getTranslatedStatus() {
-		return _t("Namespace." . strtoupper($this->Status), $this->Status);
-	}
-    
+        return _t("Namespace." . strtoupper($this->Status), $this->Status);
+    }
+
     protected function generate_order_number() {
         $id = str_pad($this->ID, 8,  "0");
-        
-        $guidText = 
-            substr($id, 0, 4) . '-' . 
-            substr($id, 4, 4) . '-' . 
+
+        $guidText =
+            substr($id, 0, 4) . '-' .
+            substr($id, 4, 4) . '-' .
             rand(1000,9999);
 
         // Work out if an order prefix string has been set in siteconfig
         $config = SiteConfig::current_site_config();
-        
+
         $guidText = ($config->OrderPrefix) ? $config->OrderPrefix . '-' . $guidText : $guidText;
 
         return $guidText;
     }
-    
+
     public function onBeforeDelete() {
         // Delete all items attached to this order
         foreach($this->Items() as $item) {
             $item->delete();
         }
-        
+
         parent::onBeforeDelete();
     }
-    
+
     public function onAfterWrite() {
         parent::onAfterWrite();
-        
+
         // Check if an order number has been generated, if not, add it and save again
         if(!$this->OrderNumber) {
             $this->OrderNumber = $this->generate_order_number();
             $this->write();
         }
-        
+
         // Deal with sending the status email
         if($this->isChanged('Status') && in_array($this->Status, array('paid','processing','dispatched')) ) {
             $siteconfig = SiteConfig::current_site_config();
-            
+
             $subject = "Order {$this->OrderNumber} {$this->Status}";
             $from =  $siteconfig->Title . "<{$siteconfig->EmailFromAddress}>";
-            
+
             $vars = array(
                 'Order' => $this,
                 'SiteConfig' => $siteconfig
             );
-            
+
             // Deal with customer email
             if($siteconfig->sendCommerceEmail('Customer', $this->Status)) {
                     $body = $this->renderWith('OrderEmail_Customer', $vars);
                     $email = new Email($from,$this->BillingEmail,$subject,$body);
                     $email->sendPlain();
             }
-            
+
             // Deal with vendor email
             if($siteconfig->sendCommerceEmail('Vendor', $this->Status)) {
                     switch($this->Status) {
                         case 'paid':
-                            $email = $siteconfig->PaidEmailAddress;
+                            $email_to = $siteconfig->PaidEmailAddress;
                         case 'processing':
-                            $email = $siteconfig->ProcessingEmailAddress;
+                            $email_to = $siteconfig->ProcessingEmailAddress;
                         case 'dispatched':
-                            $email = $siteconfig->DispatchedEmailAddress;
+                            $email_to = $siteconfig->DispatchedEmailAddress;
                     }
-                    
-                    if(isset($email)) {
+
+                    if(isset($email_to)) {
                             $body = $this->renderWith('OrderEmail_Vendor', $vars);
                             $email = new Email($from,$email,$subject,$body);
                             $email->sendPlain();
                     }
             }
-            
-            
+
+
         }
     }
-    
+
     protected function onAfterDelete() {
         parent::onAfterDelete();
-        
+
         foreach ($this->Items() as $item) {
             $item->delete();
         }
     }
-    
+
     public function canCreate($member = null) {
         return false;
     }
