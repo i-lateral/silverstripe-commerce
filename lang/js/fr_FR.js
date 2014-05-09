cff3ec3bd57390c866127e6e878264ef38e4c595
Morven Lewis-Everley <morven@i-lateral.com>
2014-05-09 15:37:06 +0100

    Update translation files

--- lang/js/fr_FR.js
+++ lang/js/fr_FR.js
@@ -1,4 +1,4 @@
 ss.i18n.addDictionary('FR_fr', {
     // Cart Page Translations
-    'COMMERCE.CART.NOPOSTAGE' : 'Vous devez sélectionner une région de livraison pour continuer'
+    'COMMERCE.CART.NOPOSTAGE' : 'Vous devez s&eacute;lectionner une r&eacute;gion de livraison pour continuer'
 });
