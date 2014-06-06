6e8ef5bdf99220d3b531829b447650b92321b86a
Morven Lewis-Everley <morven@i-lateral.com>
2014-06-06 17:31:38 +0100

    Added draft of basic paypal integration

---
+++ code/model/payment/PayPal.php
@@ -0,0 +1,38 @@
+<?php
+
+class PayPal extends CommercePaymentMethod {
+
+    public static $handler = "PayPalHandler";
+
+    public $Title = 'PayPal';
+
+    public $Icon = 'commerce/images/paypal-small.png';
+
+    private static $db = array(
+        'BusinessID' => 'Varchar(99)'
+    );
+
+    public function getCMSFields() {
+        $fields = parent::getCMSFields();
+
+        if($this->ID) {
+            $fields->removeByName("URL");
+
+            $fields->addFieldToTab(
+                "Root.Main",
+                TextField::create('BusinessID', 'Business ID'),
+                "Summary"
+            );
+        }
+
+        return $fields;
+    }
+
+    public function onBeforeWrite() {
+        parent::onBeforeWrite();
+
+        $this->CallBackSlug = (!$this->CallBackSlug) ? 'paypal' : $this->CallBackSlug;
+
+        $this->Summary = (!$this->Summary) ? "Pay with PayPal" : $this->Summary;
+    }
+}
