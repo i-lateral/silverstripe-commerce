7d5e681dc0c6f43eb7a1c8c74d66c5abede8c0ac
Morven Lewis-Everley <morven@i-lateral.com>
2014-09-18 10:03:54 +0100

    Add a basic items ordered report

---
+++ code/reports/OrderItemReportItem.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Item that can be loaded into an OrderItem report
+ *
+ */
+class OrderItemReportItem extends Object {
+
+    public $ClassName = "OrderItemReportItem";
+
+    public $SKU;
+    public $Details;
+    public $OrderNumber;
+    public $Price;
+    public $Quantity;
+
+    public function canView($member = null) {
+        return true;
+    }
+}
