730c920c3dda0b3e72d346ba3b1bc96c0bc8d1eb
Morven Lewis-Everley <morven@i-lateral.com>
2015-02-19 08:28:56 +0000

    Ensure config updates are not run if we are doing dev/build (this seems to generate an error if siteconfig isn't updated in the database)

--- code/extensions/CommerceControllerExtension.php
+++ code/extensions/CommerceControllerExtension.php
@@ -24,19 +24,21 @@ class CommerceControllerExtension extends Extension {
             if(!$upgraded) user_error("Could not upgrade the Commerce module, please check the documentation on upgrading.");
         }
 
-        if(class_exists('Subsite') && Subsite::currentSubsite()) {
-            // Set the location
-            i18n::set_locale(Subsite::currentSubsite()->Language);
-        }
+        if($controller != "DevelopmentAdmin" && $action != "build") {
+            if(class_exists('Subsite') && Subsite::currentSubsite()) {
+                // Set the location
+                i18n::set_locale(Subsite::currentSubsite()->Language);
+            }
+            
+            if(class_exists('SiteConfig') && $config = SiteConfig::current_site_config()) {
+                $currency = $config->Currency;
+                $currency_codes = Commerce::config()->currency_symbols;
 
-        if(class_exists('SiteConfig') && $config = SiteConfig::current_site_config()) {
-            $currency = $config->Currency;
-            $currency_codes = Commerce::config()->currency_symbols;
-
-            if($currency && array_key_exists($currency, $currency_codes)) {
-                // Make sure we set the default currency setting
-                Config::inst()->update('Currency', 'currency_symbol', $currency_codes[$currency]);
-                Config::inst()->update('Checkout', 'currency_symbol', $currency_codes[$currency]);
+                if($currency && array_key_exists($currency, $currency_codes)) {
+                    // Make sure we set the default currency setting
+                    Config::inst()->update('Currency', 'currency_symbol', $currency_codes[$currency]);
+                    Config::inst()->update('Checkout', 'currency_symbol', $currency_codes[$currency]);
+                }
             }
         }
     }
