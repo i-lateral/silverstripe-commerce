6e7fb344e70533e438f76e6e52eea099ad0a97fd
Morven <morven@i-lateral.com>
2016-10-27 22:54:41 +0100

    Add stock level to catalogue screen

--- code/extensions/CommerceCatalogueProductExtension.php
+++ code/extensions/CommerceCatalogueProductExtension.php
@@ -6,12 +6,35 @@
  * @author ilateral (http://ilateralweb.co.uk)
  * @package commerce
  */
-class CommerceCatalogueProductExtension extends DataExtension {
+class CommerceCatalogueProductExtension extends DataExtension
+{
+    
     private static $db = array(
         "StockLevel" => "Int",
         "PackSize" => "Int",
         "Weight" => "Decimal"
     );
+
+    private static $summary_fields = array(
+        "CMSThumbnail"  => "Thumbnail",
+        "ClassName"     => "Product",
+        "StockID"       => "StockID",
+        "Title"         => "Title",
+        "BasePrice"     => "Price",
+        "TaxRate.Amount"=> "Tax Percent",
+        "CategoriesList"=> "Categories",
+        "StockLevel"    => "Stock",
+        "Disabled"      => "Disabled"
+    );
+
+
+    /**
+     * Reset summary fields to new default
+     *
+     */
+    public function updateSummaryFields(&$fields) {
+        $fields = Config::inst()->get($this->class, 'summary_fields');
+    }
     
     /**
      * Get a list of suitable shipping bands
